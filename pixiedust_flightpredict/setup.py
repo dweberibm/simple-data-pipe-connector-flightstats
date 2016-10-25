e97ec4469dc7d822fdba26a47f2efafc080fec83
Daniel Weber <daniel.weber@ch.ibm.com>
2016-10-25 09:30:18 +0200

    Changed GIT Path

--- pixiedust_flightpredict/setup.py
+++ pixiedust_flightpredict/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup,find_packages
 setup(name='pixiedust_flightpredict',
       version='0.3',
       description='Flight delay predictor application with PixieDust',
-      url='https://github.com/ibm-cds-labs/simple-data-pipe-connector-flightstats/tree/master/pixiedust_flightpredict',
+      url='https://github.com/dweberibm/simple-data-pipe-connector-flightstats/tree/master/pixiedust_flightpredict',
       install_requires=['pixiedust'],
       author='David Taieb',
       author_email='david_taieb@us.ibm.com',
